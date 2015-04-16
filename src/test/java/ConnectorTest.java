47abe27b69b78331b6ca606455ee60003c5d0159
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2015-04-15 22:24:55 -0400

    API Test & Initial Code

---
+++ src/test/java/ConnectorTest.java
@@ -0,0 +1,30 @@
+/*
+    Copyright 2015 Mahesh Khanwalkar
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+import com.inixsoftware.haggregate.connector.AggregateConnector;
+
+import java.io.IOException;
+
+public class ConnectorTest
+{
+    public static void main(String[] args) throws IOException
+    {
+        AggregateConnector connector = new AggregateConnector("localhost", 8556);
+        connector.connect();
+
+        connector.shutdown();
+    }
+}
